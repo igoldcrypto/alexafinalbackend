d4df4446b4265d8dc11556e720fb851384db7b13
igoldcrypto <129389664+igoldcrypto@users.noreply.github.com>
2023-06-30 17:13:31 +0000

    shortrecords

--- Models/ShortRecord.js
+++ Models/ShortRecord.js
@@ -42,6 +42,10 @@ const ShortRecord = mongoose.Schema({
         type:"Bool",
         default:true
     },
+    MyWithdrawals: {
+        type: "Number",
+        default: 0,
+      },
 },
 {
   timestamps: true
